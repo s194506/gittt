e671f52139503b698d9905b7e174f716d6806b8a
Mateusz Sajler <mateusz.sajler@gmail.com>
2019-05-21 13:57:15 +0200

    drugi commit

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-
-
 function myInfo() {
 
 	var info =  'Nazywam się Mateusz Sajler i jestem studentem Uniwersytetu Ekonomicznego'
 
 	window.alert(info);
+	
+}
+function info() {
+
+	var informacja =  'Nawet mi to działa'
+
+	window.alert(informacja);
 	
 }
