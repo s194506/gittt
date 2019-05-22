d76918215782ce8b53000131fa869ae1a7cc17a2
s194506 <kamil.kr@poczta.fm>
2019-05-22 12:26:05 +0200

    a

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-function myInfo() {
+﻿function myInfo() {
 
-	var info =  'Nazywam się Mateusz Sajler i jestem studentem Uniwersytetu Ekonomicznego'
+	//var info =  'Nazywam się Mateusz Sajler i jestem studentem Uniwersytetu Ekonomicznego'
 
 	window.alert(info);
 	
